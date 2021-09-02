c24238521860938ea354ad5d3c3dec7206536cfe
Elias Junior <48299934+EliasJunior87@users.noreply.github.com>
2021-09-02 15:59:01 -0300

    Add files via upload

---
+++ Projeto Acerte o número.py
@@ -0,0 +1,36 @@
+# Projeto Acerte o Número
+
+# Crie um programa simples que gere números inteiros aleatórios entre 1-50,
+# que peça um número do usuário e verifique se o valor inserido é igual ao aleatório.
+# O programa deve ser repetido 15 vezes. Se o usuário digitar um número fora do intervalo válido
+# o programa deverá ser encerrado. Se o usuário acertar o número aleatório mostre uma mensagem personalizada na tela,
+# a cada laço de repetição.E caso o usuário não acerte, mostre na tela se o número que ele digitou foi maior ou menor
+# que o número aleatório. Ao final mostre a quantidade de acertos.
+
+from random import randint
+
+acertos = 0
+
+for num in range(1, 16):
+    print(f'Jogada {num}')
+    bot = randint(1, 50)
+    escolha_usuario = int(input('Digite um número entre 1-50: '))
+
+    if escolha_usuario in range(1, 51):
+        if escolha_usuario > bot:
+            print('Você Digitou um número maior que o aleatório')
+            print(f'Número Aleatório: {bot}')
+
+        elif escolha_usuario < bot:
+            print('Você digitou um número menor que o aleatório')
+            print(f'Número Aleatório: {bot}')
+
+        else:
+            print('Você acertou!')
+            acertos += 1
+
+    else:
+        print('Fim.')
+        break
+
+print(f'Número de acertos: {acertos}')
